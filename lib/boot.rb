1b20c42bd09f2a5357ff42503a4fdf20416f0771
Marcin <marcin@opensoftware.pl>
2020-01-31 13:13:24 +0100

    Add custom ElasticSearch config

--- lib/boot.rb
+++ lib/boot.rb
@@ -1,2 +1,22 @@
+require 'yaml'
+require 'stretcher'
+
 Dir["#{LIB_PATH}/schemas/*.rb"].each { |file| require file }
 Dir["#{LIB_PATH}/schemas/services/*.rb"].each { |file| require file }
+
+es_config = YAML.load_file("#{ROOT_PATH}/config/elastic_search.yml")
+es = Stretcher::Server.new('http://es01:9200')
+es_updated = false
+
+until es_updated
+  begin
+    es_config.each do |index, settings|
+      es.index(index).update_settings(settings)
+    end
+    es_updated = true
+  rescue Faraday::Error::ConnectionFailed
+    sleep 1
+  rescue Stretcher::RequestError::NotFound => e
+    es.index(e.http_response.env[:body]['error']['index']).create
+  end
+end
