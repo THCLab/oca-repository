5962acee170649fb871a1ff0d26d256470395fa0
Marcin <marcin@opensoftware.pl>
2020-02-20 12:47:22 +0100

    Handle uploading zip file

--- Gemfile
+++ Gemfile
@@ -4,3 +4,5 @@ gem 'base58', '~> 0.2'
 gem 'faraday', '0.8.9'
 gem 'roda', '~> 3.22'
 gem 'stretcher', '~> 1.21'
+
+gem "rubyzip", "~> 2.2"
--- lib/schemas/services/v2/import_schema_service.rb
+++ lib/schemas/services/v2/import_schema_service.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'zip'
+
 module Schemas
   module Services
     module V2
@@ -15,10 +17,9 @@ module Schemas
           params = validate(raw_params)
           type = params[:file][:filename].split('.').last
           if type == 'json'
-            schema_base = JSON.parse(params[:file][:tempfile].read)
-            hashlink = hashlink_generator.call(schema_base)
-            es.index(:schema_base).bulk_index([schema_base.merge(_id: hashlink)])
-            hashlink
+            store_json(params[:file][:tempfile])
+          elsif type == 'zip'
+            store_zip(params[:file][:tempfile])
           end
         end
 
@@ -27,6 +28,53 @@ module Schemas
           {
             file: params['file']
           }
+        end
+
+        private def store_json(file)
+          schema_base = JSON.parse(file.read)
+          hashlink = hashlink_generator.call(schema_base)
+          es.index(:schema_base).bulk_index(
+            [schema_base.merge(_id: hashlink)]
+          )
+          hashlink
+        end
+
+        private def store_zip(file)
+          schema = extract_zip(file)
+          %i[schema_base overlay].each do |index|
+            es.index(index).bulk_index(
+              schema[index].map do |hashlink, content|
+                content.merge(_id: hashlink)
+              end
+            )
+          end
+          branch = {
+            schema_base: schema[:schema_base].keys.first,
+            overlays: schema[:overlay].keys.sort
+          }
+          branch_hashlink = hashlink_generator.call(branch)
+          es.index(:branch).bulk_index(
+            [branch.merge(_id: branch_hashlink)]
+          )
+          branch_hashlink
+        end
+
+        private def extract_zip(file)
+          schema = { overlay: {} }
+          Zip::File.open(file) do |zip|
+            zip.each do |entry|
+              next unless entry.ftype == :file
+              content = JSON.parse(entry.get_input_stream.read)
+              hashlink = hashlink_generator.call(content)
+              type = entry.name.split('/').size == 1 ? :schema_base : :overlay
+              if type == :schema_base
+                schema[:schema_base] = { hashlink => content }
+              elsif type == :overlay
+                schema[:overlay].merge!(hashlink => content)
+              end
+            end
+          end
+          schema
         end
       end
     end
