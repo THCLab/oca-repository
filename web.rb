09429893298191289d5ab782c6667a56931da23d
Marcin <marcin@opensoftware.pl>
2020-04-14 18:41:12 +0200

    Add /api to URI

--- web.rb
+++ web.rb
@@ -13,10 +13,6 @@ class Web < Roda
   route do |r|
     es = Stretcher::Server.new('http://es01:9200')
 
-    r.root do
-      r.redirect('/schemas')
-    end
-
     r.on 'schemas' do
       r.get String do |id|
         service = Schemas::Services::GetSchemaService.new(es)
@@ -38,35 +34,37 @@ class Web < Roda
       end
     end
 
-    r.on 'v2' do
-      r.on 'schemas' do
-        r.on String do |namespace|
-          r.get String do |dri|
-            service = Schemas::Services::V2::GetSchemaService.new(es)
-            service.call(namespace + '/' + dri)
+    r.on 'api' do
+      r.on 'v2' do
+        r.on 'schemas' do
+          r.on String do |namespace|
+            r.get String do |dri|
+              service = Schemas::Services::V2::GetSchemaService.new(es)
+              service.call(namespace + '/' + dri)
+            end
+
+            r.get do
+              service = Schemas::Services::V2::SearchSchemasService.new(es)
+              service.call(r.params.merge(namespace: namespace))
+            end
+
+            r.post do
+              service = Schemas::Services::V2::ImportSchemaService.new(
+                es, ::Schemas::HashlinkGenerator
+              )
+              dri = service.call(namespace, r.params)
+
+              {
+                DRI: dri,
+                url: "#{r.base_url}/api/v2/schemas/#{namespace}/#{dri}"
+              }
+            end
           end
 
           r.get do
             service = Schemas::Services::V2::SearchSchemasService.new(es)
-            service.call(r.params.merge(namespace: namespace))
+            service.call(r.params)
           end
-
-          r.post do
-            service = Schemas::Services::V2::ImportSchemaService.new(
-              es, ::Schemas::HashlinkGenerator
-            )
-            dri = service.call(namespace, r.params)
-
-            {
-              DRI: dri,
-              url: "#{r.base_url}/api/v2/schemas/#{namespace}/#{dri}"
-            }
-          end
-        end
-
-        r.get do
-          service = Schemas::Services::V2::SearchSchemasService.new(es)
-          service.call(r.params)
         end
       end
     end
