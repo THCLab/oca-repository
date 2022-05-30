50c1f4953764d07c4cfd976016e0f89f53e47644
Marcin Olichwiruk <marcin@opensoftware.pl>
2022-05-30 23:24:32 +0200

    Archive OCA Bundle as flat zip

--- lib/v0_1/oca/services/generate_archive_service.rb
+++ lib/v0_1/oca/services/generate_archive_service.rb
@@ -36,26 +36,59 @@ module V01
         end
 
         private def pack_to_zip(schema)
-          capture_base, overlays = destruct_schema(schema)
+          capture_base, overlays, references = destruct_schema(schema)
           capture_base_sai = hashlink_generator.call(capture_base)
 
+          meta = { root: capture_base_sai, files: {} }
+          meta[:files]["capture_base-0"] = capture_base_sai
+
           io_stream = Zip::OutputStream.write_buffer do |zio|
-            zio.put_next_entry("#{capture_base_sai}/")
             zio.put_next_entry(capture_base_sai + '.json')
             zio.write(JSON.generate(capture_base))
             overlays.each do |overlay|
               overlay_sai = hashlink_generator.call(overlay)
-              zio.put_next_entry("#{capture_base_sai}/#{overlay_sai}.json")
+              meta[:files][parse_meta_key(overlay)] = overlay_sai
+              zio.put_next_entry("#{overlay_sai}.json")
               zio.write(JSON.generate(overlay))
             end
+
+            if references
+              references.entries.map { |e| e[1] }.each_with_index do |reference, i|
+                cb_sai = hashlink_generator.call(reference[:capture_base])
+                meta[:files]["capture_base-#{i + 1}"] = cb_sai
+                zio.put_next_entry("#{cb_sai}.json")
+                zio.write(JSON.generate(reference[:capture_base]))
+                reference[:overlays].each do |overlay|
+                  overlay_sai = hashlink_generator.call(overlay)
+                  meta[:files][parse_meta_key(overlay)] = overlay_sai
+                  zio.put_next_entry("#{overlay_sai}.json")
+                  zio.write(JSON.generate(overlay))
+                end
+              end
+            end
+
+            zio.put_next_entry('meta.json')
+            zio.write(JSON.pretty_generate(meta))
           end
           io_stream.string
+        end
+
+        private def parse_meta_key(overlay)
+          language = overlay['language']
+          overlay_type = overlay['type'].split('/')[2]
+          key = "[#{overlay['capture_base']}] #{overlay_type}"
+
+          return key unless language
+          key += " (#{language})"
+
+          key
         end
 
         private def destruct_schema(schema)
           [
             schema[:capture_base],
-            schema[:overlays]
+            schema[:overlays],
+            schema[:references]
           ]
         end
       end
