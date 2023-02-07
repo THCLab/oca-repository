557bc05fad3ec558a85e9c5bd4e7ac0cd03ba331
Marcin Olichwiruk <21108638+olichwiruk@users.noreply.github.com>
2023-02-07 15:12:26 +0100

    feat: calculate SAI for schemas with SAID

--- lib/common/sai_generator.rb
+++ lib/common/sai_generator.rb
@@ -8,9 +8,12 @@ require 'digest/blake3'
 module Common
   class SaiGenerator
     def self.call(schema)
+      schema_tmp = schema.clone
+      schema_tmp['digest'] = '############################################' if schema_tmp['digest']
+
       'E' +
         Base64.urlsafe_encode64(
-          Digest::BLAKE3.digest(JSON.generate(schema)),
+          Digest::BLAKE3.digest(JSON.generate(schema_tmp)),
           padding: false
         )
     end
